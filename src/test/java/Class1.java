b664d155ed94236208400d75ee6f129ec6fe6518
codefish <contact@codefish.io>
2024-03-06 11:09:37 -0600

    my first commit

--- src/test/java/Class1.java
+++ src/test/java/Class1.java
@@ -1,6 +1,6 @@
 public class Class1 {
     public static void main(String[] args) {
         ///first note
-        System.out.println("First note");
+        System.out.println("First git note");
     }
 }
